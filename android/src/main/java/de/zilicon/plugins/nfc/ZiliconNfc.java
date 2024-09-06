1d251e949bb8f170f4803b0448ba483dfcf62e98
Sam White <samjwhite004@gmail.com>
2024-09-06 15:31:00 +1200

    Initial commit

---
+++ android/src/main/java/de/zilicon/plugins/nfc/ZiliconNfc.java
@@ -0,0 +1,11 @@
+package de.zilicon.plugins.nfc;
+
+import android.util.Log;
+
+public class ZiliconNfc {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
---
+++ android/src/main/java/de/zilicon/plugins/nfc/ZiliconNfcPlugin.java
@@ -0,0 +1,22 @@
+package de.zilicon.plugins.nfc;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "ZiliconNfc")
+public class ZiliconNfcPlugin extends Plugin {
+
+    private ZiliconNfc implementation = new ZiliconNfc();
+
+    @PluginMethod
+    public void echo(PluginCall call) {
+        String value = call.getString("value");
+
+        JSObject ret = new JSObject();
+        ret.put("value", implementation.echo(value));
+        call.resolve(ret);
+    }
+}
